1cc46c0df9c3099fd977e2dd693319a6faca9243
Александр <45181085+kssndr@users.noreply.github.com>
2020-01-06 23:26:40 +0300

    dinamic template added

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Item
 
 
 def item_list(request):
-    return render(request, 'landing/item_list.html', {})
+	items = Item.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'landing/item_list.html', {'items':items})
+
+	
