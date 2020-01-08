1b543dd49d7dd6f9feb3284ab6b82f3f0ea909dd
Александр <45181085+kssndr@users.noreply.github.com>
2020-01-08 16:42:48 +0300

    CVB_done

--- landing/urls.py
+++ landing/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+# from django.conf.urls import patterns
+from landing.views import ItemList
 
+
+# urlpatterns = [
+#     path('', views.item_list, name='item_list'),
+# ]
 
 urlpatterns = [
-    path('', views.item_list, name='item_list'),
+    path('', ItemList.as_view()),
 ]
